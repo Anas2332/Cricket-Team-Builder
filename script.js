76c22242fd6878622c73a9c0c460316adef08cc7
Anas2332 <amunir2332@gmail.com>
2025-02-12 19:11:29 +0500

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.querySelector(".btn").addEventListener("click" , ()=>{
-    document.getElementById("reg").classList.add("hidden")
-    document.getElementById("section").style.display = "block"
+    let teamName = document.querySelector(".input").value
+    console.log(teamName);
+    if (teamName === "") {
+        alert("Please enter your team name!!!")
+        return;
+    }
+    document.getElementById("reg").classList.add("hidden");
+    document.querySelector(".teamName").innerText = teamName;
+    document.getElementById("section").style.display = "block";
 })
 document.querySelector(".downarrow").addEventListener("click" , ()=>{
     document.querySelector(".categories").classList.toggle("show")
